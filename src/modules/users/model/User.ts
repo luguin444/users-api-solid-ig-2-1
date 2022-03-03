3e16781ac06b330751b99e0a86b01da38b96d41e
Luis Gustavo <luguin444@gmail.com>
2022-03-03 05:04:12 -0300

    feat: build model User

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,22 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+  name: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+  admin: boolean;
+
+  private;
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+      this.created_at = new Date();
+      this.updated_at = new Date();
+      this.admin = false;
+    }
+  }
 }
 
 export { User };
