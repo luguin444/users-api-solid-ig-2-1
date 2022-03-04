d59ade901858e252fd1e80ebf31f818aef765b62
Luis Gustavo <luguin444@gmail.com>
2022-03-03 21:00:07 -0300

    feat: create docs with swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerSetUpFile from "../swaggerSetUpFile.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSetUpFile));
 
 app.use("/users", usersRoutes);
 
